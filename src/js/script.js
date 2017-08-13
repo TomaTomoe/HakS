3e1471eb643ecc2f337b0104144b7877b6a884bb
xxx <xxx@gmail.com>
2017-08-14 02:29:11 +0300

    team slider in process

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,23 @@
 $(document).ready(function () {
-    $(document).ready(function() {
+    var swiper = new Swiper('#swiper-container-partners', {
+        pagination: '.swiper-pagination',
+        slidesPerView: 4,
+        paginationClickable: true,
+        spaceBetween: 30,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        pagination: '.swiper-pagination',
+        paginationType: 'fraction'
+    });
 
+    var swiper2 = new Swiper('#swiper-container-team', {
+        pagination: '.swiper-pagination',
+        slidesPerView: 3,
+        paginationClickable: true,
+        spaceBetween: 50,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        pagination: '.swiper-pagination',
+        paginationType: 'fraction'
     });
 });
